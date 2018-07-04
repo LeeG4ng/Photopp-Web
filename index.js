dfe5c6ccecfbba66ee3b09e292b17b4392c4bbfb
LeeG4ng <LeeG4ng@qq.com>
2018-07-04 19:58:57 +0800

    body

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer'); 
 var userRouter = require('./route/user');
 var imageRouter = require('./route/image');
 
 // 设定port变量，意为访问端口
 app.set('port', process.env.PORT || 8086);
 app.listen(app.get('port'));
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
 
 app.use('/user', userRouter);
 app.use('/image', imageRouter);
