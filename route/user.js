02ca9a1392fffe24e23f738e2d39c363076a647c
LeeG4ng <LeeG4ng@qq.com>
2018-07-04 20:39:16 +0800

    d

--- route/user.js
+++ route/user.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
-var userFunction = require('../function/user');
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://127.0.0.1:27017';
 
 router.post('/login', function(req, res, next) {
     console.log('/user/login');
@@ -19,13 +19,26 @@ router.post('/register', function(req, res, next) {
     console.log('/user/register');
     console.log(req.body);
 
-    var msg = userFunction.register(req.body['username'], req.body['password']);
-    console.log(msg);
-    if (msg['err']) {//错误
-        res.status(403).send(msg['err']);
-    } else {//正确send jwt
-        res.send();
-    }
+    MongoClient.connect(url, function(err, db) {
+        if(err) throw err;
+        var dbase = db.db('Photopp');
+        console.log('db connected');
+        var col = dbase.collection('user');
+        col.find({'username':username}).toArray(function(err, results) {
+            if (err) throw err;
+            if(results.length > 0) {//用户名已存在
+                res.status(403).json({'error':'用户名已存在','jwt':null});
+            } else {
+                console.log('可注册');
+                col.insertOne({'username':username, 'password':password}, function(inserterr, res) {
+                    if(inserterr) throw inserterr;
+                    console.log('注册成功：'+username+' '+password);
+                })
+                res.send({'error':null, 'jwt':null});
+            }
+            db.close();
+        })
+    });
 });
 
 module.exports = router;
