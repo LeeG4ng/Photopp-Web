7722d6b743c017a2d467c52648317cec87c578a1
LeeG4ng <LeeG4ng@qq.com>
2018-07-05 09:49:30 +0800

    decode

--- route/image.js
+++ route/image.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jwt-simple');
+var secret = 'PhotoppWebServer';
 
-
-router.get('/image', function(req, res, next) {
-  res.send('image');
-  console.log('get image');
+router.post('/image', function(req, res, next) {
+    var token = req.body['jwt'];
+    var username = jwt.decode(token, secret)['iss'];
+    console.log(username);
+    console.log('get image');
 });
+
+
 
 module.exports = router;
