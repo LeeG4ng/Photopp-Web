96bc3ca3ad840a971ee67117a1b516a005a0b2df
LeeG4ng <LeeG4ng@qq.com>
2018-07-06 16:42:40 +0800

    dd

--- route/image.js
+++ route/image.js
@@ -5,7 +5,7 @@ const secret = 'PhotoppWebServer';
 var MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://127.0.0.1:27017';
 var request = require('request');
-var exif = require('exif-parser');
+var exif = require('exif').ExifImage;
 const map_key = '2c541c4ac6a4392c10bf0934274f44ff';
 const api_key = 'NER771cf0iCV_Mw_D6whO7BVZWdQe9jR';
 const api_secret = '0PIOsHqmIQpDC6eJhUJXpLMjAfPrA2I1'
@@ -21,7 +21,7 @@ router.post('/image', function(req, res, next) {
 });
 
 router.post('/upload', function(req, res) {
-    console.log(req.body);
+    // console.log(req.body);
     var token = req.body['jwt'];
     var username = jwt.decode(token, secret)['iss'];
     var image = req.body['image'];
@@ -31,7 +31,10 @@ router.post('/upload', function(req, res) {
     //获得经纬度参数
     var basecode = image.split('base64,')[1];//删除前缀的base64
     var buffer = new Buffer(basecode, 'base64');
-    var parser = exif.create(buffer);
+    new exif(buffer, function(exif_error, exif_data) {
+        if (exif_error) throw exif_error;
+        console.log(exif_data);
+    })
     // var result = parser.parse();
     // console.log(result);
 
@@ -55,10 +58,10 @@ router.post('/upload', function(req, res) {
     
     // res.send(GPS);
 
-    var pramas = {api_key:api_key,api_secret:api_secret,'image_base64':basecode,};
-    request.post({url:face_url, form:pramas}, function(face_err, face_res, face_body) {
-        console.log(face_body);
-    });
+    // var pramas = {api_key:api_key,api_secret:api_secret,'image_base64':basecode,};
+    // request.post({url:face_url, form:pramas}, function(face_err, face_res, face_body) {
+    //     console.log(face_body);
+    // });
     // form.submit(face_url, function(errr, ress) {
     //     console.log(ress.statusMessage);
     // });
