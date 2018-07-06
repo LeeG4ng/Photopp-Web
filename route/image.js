b573b9921550f558431870ed55c90312cff053cf
LeeG4ng <LeeG4ng@qq.com>
2018-07-06 16:16:53 +0800

    ds

--- route/image.js
+++ route/image.js
@@ -55,17 +55,17 @@ router.post('/upload', function(req, res) {
     
     // res.send(GPS);
 
-    var pramas = {api_key:api_key,api_secret:api_secret,'image_file':buffer,};
-    // request.post({url:face_url, formData:pramas}, function(face_err, face_res, face_body) {
-    //     console.log(face_body);
-    // });
+    var pramas = {api_key:api_key,api_secret:api_secret,'image_base64':basecode,};
+    request.post({url:face_url, form:pramas}, function(face_err, face_res, face_body) {
+        console.log(face_body);
+    });
     // form.submit(face_url, function(errr, ress) {
     //     console.log(ress.statusMessage);
     // });
-    needle.post(face_url, pramas, function(face_err, face_res) {
-        if(face_err) throw face_err;
-        console.log(face_res.body);
-    })
+    // needle.post(face_url, pramas, function(face_err, face_res) {
+    //     if(face_err) throw face_err;
+    //     console.log(face_res.body);
+    // })
     res.end();
 });
 
