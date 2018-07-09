50d50a50bd50261bc258f75349438fb34d6fabed
LeeG4ng <LeeG4ng@qq.com>
2018-07-09 09:03:18 +0800

    das

--- route/image.js
+++ route/image.js
@@ -31,7 +31,7 @@ router.post('/upload', function(req, res) {
     // var username = jwt.decode(token, secret)['iss'];
     var image = req.body['image'];
     // console.log('user ' + username +' upload image.');
-    console.log(image);
+    // console.log(image);
     var GPS = req.body['GPS'];
 
     //获得经纬度参数
