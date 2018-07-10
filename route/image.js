2d94b8552c955df102dbcad1e651ee9628c72535
LeeG4ng <LeeG4ng@qq.com>
2018-07-10 10:58:12 +0800

    ds

--- route/image.js
+++ route/image.js
@@ -83,24 +83,8 @@ router.post('/upload', function(req, res) {
     }).catch(function(face_err) {
         if (face_err) throw face_err;
     })
-    /*
-    if (GPS) {//调用高德接口
-        var map_url = 'http://restapi.amap.com/v3/geocode/regeo?key='+map_key+'&location='+GPS;
-        request(map_url, function(map_err, map_res, map_body) {
-            if (map_err) throw map_err;
-            var json = JSON.parse(map_body);
-            console.log(json);
-            var location = json['regeocode']['formatted_address'];
-            console.log(location);
-            res.send(location);
-            
-        });
-    }*/
-    
-    // res.send(GPS);
 
 
-    res.end();
 });
 
 router.post('/delete', function(req, res) {
